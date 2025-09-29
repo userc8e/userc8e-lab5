ff3d81e074011523eb52a0c94b1c6ea8e861ad4e
userc8e <caitlynquack@gmail.com>
2025-09-28 23:02:33 -0400

    implemented encrypt function

--- Enigma.java
+++ Enigma.java
@@ -20,13 +20,39 @@ public class Enigma{
 
 
     public String decrypt(String message){        
-        //TODO
+        char[] msg = message.toCharArray(); //message in char array form
+        String answer = ""; //answer that will be returned at end of function
     }
 
 
     
     public String encrypt(String message){
-        //TODO
+        char[] msg = message.toCharArray(); //message in char array form
+        String answer = ""; //answer that will be returned at the end of function
+
+        // goes through for loop to encrypt each char of message
+        for (int i = 0; i < msg.length; i++) {
+            char val = msg[i]; //the value being converted
+            int idx; //index on rotor
+
+            // goes through rotor logic to find the encrypted value
+            idx = rotors[0].indexOf(val);
+            val = rotors[1].charAt(idx);
+            idx = rotors[2].indexOf(val);
+            val = rotors[2].charAt(idx);
+
+            // adds this value to the answer
+            // (end of loop will result in a fully encrypted answer)
+            answer += val;
+
+            // rotate rotors
+            if (rotors[0].rotate()) {
+                if (rotors[1].rotate()) {
+                    rotors[2].rotate();
+                }
+            }
+        }
+        return answer; //final encrypted message
     }
 
     
